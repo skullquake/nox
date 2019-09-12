f10fc1e39a64e791d95d3160ed2f48374669f599
skullquake <ockert8080@gmail.com>
2019-09-12 19:40:09 +0200

    .

--- duktape/combo/res/cjs/app/home.js
+++ duktape/combo/res/cjs/app/home.js
@@ -1,27 +1,112 @@
 {
-	var home=function(ctlP){
+	var Home=function(ctlP){
 		this.log('Constructor()');
 		this.ctl=ctlP;
+		var UrlUtils=require('cjs/url/urlutils.js');
+		this.urlUtils=new UrlUtils();
+;		this.tpl_page=new TextDecoder("utf-8").decode(readFile('./res/tpl/page.html'));
+		this.tpl_menu=new TextDecoder("utf-8").decode(readFile('./res/tpl/menu.html'));
+		this.tpl_contents=new TextDecoder("utf-8").decode(readFile('./res/tpl/contents.html'));
+		this.tpl_card=new TextDecoder("utf-8").decode(readFile('./res/tpl/card.html'));
+		this._=require('cjs/lodash/lodash.js');
+		this.json2html=require("cjs/json2html/json2html.js");
+		var Anchor=require('cjs/wid/anchor.js');
+		var Button=require('cjs/wid/button.js');
+		var Container=require('cjs/wid/container.js');
+		var Layout=require('cjs/wid/layout.js');
+		var Menu=require('cjs/wid/menu.js');
+		var Table=require('cjs/wid/table.js');
+		var Text=require('cjs/wid/text.js');
+		var Model=require('cjs/mod/mod.js');
+		var View=require('cjs/view/view.js');
+		this.links=[
+			{'cmd':'hdlreinit'},
+			{'cmd':'home'},
+			{'cmd':'json2html'},
+			{'cmd':'json2htmlajax'},
+			{'cmd':'pgtst'},
+		];
+		this.container=new Container(null);
+		for(var i=0;i<4;i++){
+			var c=new Container(null);
+			this.container.addChild(c);
+			for(var j=0;j<4;j++){
+				var d=new Container(null);
+				c.addChild(d);
+				for(var k=0;k<2;k++){
+					var e=new Button(null);
+					e.setText('btn');
+					d.addChild(e);
+				}
+				for(var k=0;k<2;k++){
+					var f=new Anchor(null);
+					f.setCmd(this.ctl.data.cmd);
+					f.setText('anchor');
+					f.onClick=function(){
+						this.log(this.uuid+': hello:)');
+						this.setText('changed');
+					};
+					d.addChild(f);
+				}
+
+			}
+		}
 	};
-	home.prototype.src='res/cjs/cmd/cmd/home.js';
-	home.prototype.log=function(a){
+	Home.prototype.src='res/cjs/app/home.js';
+	Home.prototype.log=function(a){
 		console.log(new Date().getTime()+" "+this.src+": "+a);
 	}
-	home.prototype.data={};
-	home.prototype.exec=function(){
+	Home.prototype.data={};
+	Home.prototype.exec=function(){
 		this.log('exec()');
+		//console.log('----------------------------------------')
+		//console.log('this.container.getParent()');
+		//console.log(this.container.getParent());
+		//console.log('this.container.getChildren()');
+		//console.log(this.container.getChildren());
+		//console.log(this.container.getChildren().length);
+		//console.log('this.container.getDescendents()');
+		//console.log(this.container.getDescendents());
+		//console.log(JSON.stringify(this.container.getDescendents()));
+		//console.log(this.container.getDescendents().length);
+		//console.log('----------------------------------------')
+		//console.log(this.container.toString());
+		console.log('----------------------------------------')
+		console.log('Query');
+		console.log('----------------------------------------')
+		var id=this.urlUtils.getQueryVariable(request.getQueryString(),'id');
+		var _this=this;
+		var o=this._.find(this.container.getDescendents(), function(o) { return o.uuid==id;});
+		typeof(o)!='undefined'&&typeof(o.onClick)=='function'?o.onClick():null;
+		_response.setHeader('Content-type','text/html');
+		_response.write(
+			this._.template(this.tpl_page)(
+				{
+					'contents':
+					this._.template(
+						'<%= msg %><br/><%= links %><br/><%= test %>'
+					)(
+						{
+							'msg':this.ctl.ses.data.msg==null?'':this.ctl.ses.data.msg,
+							'links':this.json2html.transform(this.links,{'<>':'li','html':'<a href="/?cmd=${cmd}">${cmd}</a>'}),
+							'test':this.container.toString()
+						}
+					)
+				}
+			)
+		);
 	}
-	home.prototype.update=function(){
+	Home.prototype.update=function(){
 		this.data.modified=new Date().getTime();
 	}
-	home.prototype.toJson=function(){
+	Home.prototype.toJson=function(){
 		var ret={};
 		return ret;
 	}
-	home.prototype.toString=function(a,b){
+	Home.prototype.toString=function(a,b){
 		return JSON.stringify(this.toJson(),a==null?-1:a,b==null?'':b);
 	}
-	module.exports=home;
+	module.exports=Home;
 }
 
 
